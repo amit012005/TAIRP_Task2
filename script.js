ac0241955ee17fec72b9fb5f59bb3b7858647d90
amit012005 <120596857+amit012005@users.noreply.github.com>
2023-08-31 02:07:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let links = document.querySelector('.nav-links')
+let navBtn = document.querySelector('.nav-btn')
+  
+  navBtn.addEventListener('click', () => {
+    links.classList.toggle('show-links')
+  })
+  
